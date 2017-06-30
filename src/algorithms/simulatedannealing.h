9de2a1d6045cb65d0f0d0d4f3d202d29ce77eb29
Jialu Hu <jhu@nwpu.edu.cn>
2017-06-30 21:57:09 +0800

    remove conserved edges

--- src/algorithms/simulatedannealing.h
+++ src/algorithms/simulatedannealing.h
@@ -368,7 +368,7 @@ SimulatedAnnealing<RS,ST,NP,OP>::printAlignment_t(std::string& filename,
   {
       outfile2 << "Nodes in network "<<j+1 <<": " << networkpool.getGraph(j)->nodeNum << std::endl;
       outfile2 << "Edges in network "<<j+1 <<": "<< networkpool.getGraph(j)->edgeNum << std::endl;
-      outfile2 <<"Alignment edges conserved over "<<j<<" species: " << _x._numConserved[j]<<std::endl;
+      //outfile2 <<"Alignment edges conserved over "<<j<<" species: " << _x._numConserved[j]<<std::endl;
   }
   outfile2 << "Alpha parameter:" << _x._getAlpha() << std::endl;
   outfile2 << "nmax parameter:" << _Nmax << std::endl;
